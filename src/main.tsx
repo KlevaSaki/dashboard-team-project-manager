943a015c4fc12416811f99b001bdeef5d95a7ebf
Ismael Rogers <rogerus.madaki22@gmail.com>
2026-03-17 22:18:55 +0300

    feat: setup app provider and routing at root level

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
+// import ReactDOM from "react-dom/client"
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { AppProvider } from './context/AppProvider.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AppProvider>
   </StrictMode>,
 )
