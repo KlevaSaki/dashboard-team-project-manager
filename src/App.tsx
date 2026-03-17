b06bed3c9d98cebac32456b1a6a4793b884315a6
Ismael Rogers <rogerus.madaki22@gmail.com>
2026-03-17 22:33:58 +0300

    feat: implement routing for dashboard and project pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
+import type { PropsWithChildren } from 'react'
 import './App.css'
 import { AppProvider } from './context/AppProvider'
 import Dashboard from './pages/Dashboard'
+import { Route, Routes, useParams } from 'react-router-dom'
+import ProjectPage from './pages/ProjectPage'
+
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <header className="bg-white shadow p-4 font-bold">
+        Team Project Manager
+      </header>
+
+      <main>{children}</main>
+    </div>
+  )
+}
+
+//Wrapper to extract route params
+
+function ProjectWrapper() {
+  const { projectId = "" } = useParams();
+
+  if(!!projectId) {
+    return <div>Project not found</div>
+  }
+
+  return <ProjectPage projectId={projectId} />
+}
 
 function App() {
 
   return (
-    <AppProvider>
-      <Dashboard />
-    </AppProvider>
+    <Layout>
+      <Routes>
+        {/* Dashboard */}
+        <Route path='/' element={<Dashboard/>}/>
+
+        {/* Project Page */}
+        <Route path='/project/:projectId' element={<ProjectWrapper />} />
+      </Routes>
+    </Layout>
   )
 }
 
