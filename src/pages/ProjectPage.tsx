95da078f17ce57de25c2a46adf7852dfe7038986
Ismael Rogers <rogerus.madaki22@gmail.com>
2026-03-17 22:53:22 +0300

    feat: add task creation inside project board

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Column from "../components/Column";
 import { useTasks } from "../hooks/useTasks"
 
@@ -6,7 +7,16 @@ type Props = {
 }
 
 export default function ProjectPage({ projectId }: Props){
-    const { tasks } = useTasks(projectId);
+    const [ title, setTitle ] = useState("");
+
+    const { tasks, createTask } = useTasks(projectId);
+
+    function handleCreate() {
+        if(!title.trim()) return;
+
+        createTask(title);
+        setTitle("");
+    }
 
     const backlog = tasks.filter((t) => t.status === "backlog");
     const todo = tasks.filter(t => t.status === "todo");
@@ -15,12 +25,25 @@ export default function ProjectPage({ projectId }: Props){
     const done = tasks.filter(t => t.status === "done");
 
     return (
-        <div className="flex gap-4 overflow-x-auto p-4">
-            <Column title="Backlog" tasks={backlog}/>
-            <Column title="Todo" tasks={todo} />
-            <Column title="In Progress" tasks={progress} />
-            <Column title="Review" tasks={review} />
-            <Column title="Done" tasks={done} />
+        <div className="space-y-4">
+            <div className="p-4 flex gap-2">
+                <input type="text"
+                placeholder="New task..."
+                value={title}
+                onChange={e => setTitle(e.target.value)} />
+
+                <button className="bg-blue-500 text-white px-4" onClick={handleCreate}>Add Task</button>
+            </div>
+
+
+
+            <div className="flex gap-4 overflow-x-auto p-4">
+                <Column title="Backlog" tasks={backlog}/>
+                <Column title="Todo" tasks={todo} />
+                <Column title="In Progress" tasks={progress} />
+                <Column title="Review" tasks={review} />
+                <Column title="Done" tasks={done} />
+            </div>
         </div>
     )
 }
